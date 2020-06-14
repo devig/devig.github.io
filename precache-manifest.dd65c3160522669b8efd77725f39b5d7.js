f735270d79bbc5206314f92a2597272a55828eff
devig <golovnevivan@mail.ru>
2020-06-14 20:51:19 +0300

    Add files via upload

---
+++ precache-manifest.dd65c3160522669b8efd77725f39b5d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5cf88e7bb09d66f8bca201ec05f66f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c019b7531d67caa24b08",
+    "url": "/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "cf3c10146ff8b1a3e32e",
+    "url": "/static/css/main.98ba4bfb.chunk.css"
+  },
+  {
+    "revision": "c019b7531d67caa24b08",
+    "url": "/static/js/2.fccbb5c1.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.fccbb5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf3c10146ff8b1a3e32e",
+    "url": "/static/js/main.6ad6ae71.chunk.js"
+  },
+  {
+    "revision": "83c9cc866d19b7caaef9",
+    "url": "/static/js/runtime-main.bffa6175.js"
+  }
+]);
